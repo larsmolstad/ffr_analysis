b01e0804fd3680d04665743c88d6a11c60352666
Lars Molstad <lars.molstad@nmbu.no>
2021-10-17 11:03:38 +0200

    made polygon_rectangles grid with individual gaps

--- prog/polygon_utils2.py
+++ prog/polygon_utils2.py
@@ -15,7 +15,7 @@ p.rotate(.3, 0)
 p.plot(color="green")
 plt.scatter(*p.points()[0])
 plt.scatter(*p.points()[1], marker='s')
-d = p.divide_rectangle(3, gaps=(0,.1,0), other_way=True)
+d = p.divide(3, gaps=(0,.1,0), other_way=True)
 plot_rectangles(d, "0 1 2 3 4 5 6 7 8".split())
 for r in d:
     plt.scatter(*r.points()[0])
@@ -28,9 +28,11 @@ def test_point(x,y):
             print(i)
 
 test_point(.3, 10.5)
-
+#--
+p = Polygon([0,10,10,0], [20,20,40,40])
 plt.cla()
-grid = p.grid_rectangle(3,4)
+p.plot()
+grid = p.grid(3,4, ngaps=[.5,1,.5], mgaps = [.1, 1,2,.1])
 plot_rectangles(grid, range(12))
 """
 
@@ -104,7 +106,7 @@ class Polygon(object):
         self.x = np.array([0, W, W, 0]) + x0
         self. y = np.array([0, 0, L, L]) + y0
         
-    def grid_rectangle(self, m, n, mgaps=(0,0,0), ngaps=(0,0,0)):
+    def grid(self, m, n, mgaps=(0,0,0), ngaps=(0,0,0)):
         """Divides the rectangle
           p3 --- p2     
           |       |
@@ -116,12 +118,12 @@ class Polygon(object):
            r1  r2  r3
            r4  r5  r6
 
-        See divide_rectangle for description of gaps"""
-        rectlist = self.divide_rectangle(m, gaps=mgaps, other_way=True)
-        rectlists = [r.divide_rectangle(n, gaps=ngaps) for r in rectlist]
+        See divide for description of gaps"""
+        rectlist = self.divide(m, gaps=mgaps, other_way=True)
+        rectlists = [r.divide(n, gaps=ngaps) for r in rectlist]
         return sum(rectlists, [])
     
-    def divide_rectangle(self, n, other_way=False, gaps=(0,0,0)):
+    def divide(self, n, other_way=False, gaps=(0,0,0)):
         """Divides the rectangle
     
           p3 --- p2     
@@ -145,31 +147,45 @@ class Polygon(object):
               gaps[0]
             p0     p1  
         
-        gaps[1] is repeated if n>2.
+        gaps[1] is repeated if n>2 and len(gaps) == 3
         Actually, it doesn't need to be a rectangle, just a 4-gon
         """
+        if len(gaps) == 3:
+            gaps = [gaps[0]] + [gaps[1]]*(n-1) + [gaps[-1]]
         
         p = self.points()
         if other_way:
             p = [p[-1], p[0], p[1], p[2]]
         unitv1 = (p[1] - p[0])/np.linalg.norm(p[1]-p[0])
         unitv2 = (p[2] - p[3])/np.linalg.norm(p[2]-p[3])
-        p[0] += unitv1*gaps[0]
-        p[3] += unitv2*gaps[0]
-        p[1] -= unitv1*gaps[0]
-        p[2] -= unitv2*gaps[0]
-        L1 = np.linalg.norm(p[1]-p[0])
-        l1 = (L1+gaps[1])/n
-        L2 = np.linalg.norm(p[2]-p[3])
-        l2 = (L2+gaps[1])/n
-        p0s = np.array([p[0] + unitv1*l1*i for i in range(n)])
-        p1s = p0s + (l1-gaps[1])*unitv1
-        p3s = np.array([p[3] + unitv2*l2*i for i in range(n)])
-        p2s = p3s + (l2-gaps[1])*unitv2
-        print(p0s)
-        print(p1s)
-        print(p2s)
-        print(p3s)
+        # p[0] += unitv1*gaps[0]
+        # p[3] += unitv2*gaps[0]
+        # p[1] -= unitv1*gaps[-1]
+        # p[2] -= unitv2*gaps[-1]
+        # L1 = np.linalg.norm(p[1]-p[0])
+        # L2 = np.linalg.norm(p[2]-p[3])
+        # l1 = (L1 + sum(gaps[1:-2])) / n
+        # l2 = (L2 + sum(gaps[1:-2])) / n
+        # p0s = np.array([p[0] + unitv1*l1*i for i in range(n)])
+        # p1s = p0s + (l1-gaps[1])*unitv1
+        # p3s = np.array([p[3] + unitv2*l2*i for i in range(n)])
+        # p2s = p3s + (l2-gaps[1])*unitv2
+        def coll(p0, p1, gaps):
+            L = np.linalg.norm(p1 - p0)
+            unitv = (p1 - p0)/L
+            n = len(gaps) - 1
+            l = (L - np.sum(gaps))/n
+            p = p0*1
+            pp = np.zeros((n*2 , 2))
+            for i in range(n):
+                p += unitv*gaps[i]
+                pp[2*i,:] += p
+                p += unitv*l
+                pp[2*i+1,:] += p
+            return pp[::2], pp[1::2]
+
+        p0s, p1s = coll(p[0], p[1], gaps)
+        p3s, p2s = coll(p[3], p[2], gaps)
         if other_way:
             p0s, p1s, p2s, p3s = p1s, p2s, p3s, p0s
         return [Polygon((p0s[i], p1s[i], p2s[i], p3s[i])) for i in range(n)]        
@@ -216,28 +232,39 @@ class Polygon(object):
         return inside
 
     
-
-
-def plot_rectangles(rectangles, names=True, textkwargs={}, **kwargs):
-    """rectangles can be a dict or a list of rectangles. If rectangles is
+def plot_polygons(polygons, names=True, textkwargs={}, **kwargs):
+    """polygons can be a dict or a list of polygons. If polygons is
 a dict and names==True, the keys are usesd as names. names may also be
 a list"""
-    assert(isinstance(rectangles, (dict, list)))
-    if isinstance(rectangles, dict):
-        pairs = [(key, rectangles[key]) for key in list(rectangles)]
-        rectangles = [x[1] for x in pairs]
+    assert(isinstance(polygons, (dict, list)))
+    non_plottable = 0
+    plottable = 0
+    if isinstance(polygons, dict):
+        pairs = [(key, polygons[key]) for key in list(polygons)]
+        polygons = [x[1] for x in pairs]
         if names is True:
             names = [x[0] for x in pairs]
     else:
         if names is True:
-            names = range(1, len(rectangles)+1)
-    for i, r in enumerate(rectangles):
-        if callable(r): # for the pot experiment, where we don't have rectangles, but a list of functions. untested todo.
+            names = range(1, len(polygons)+1)
+    for i, r in enumerate(polygons):
+        if callable(r): # for the pot experiment, where we don't have polygons, but a list of functions. untested todo.
+            non_plottable += 1
             continue
         r.plot(text=None if not names else names[i],
                textkwargs=textkwargs,
                **kwargs)
-    
+        plottable += 1
+    if non_plottable > 0:
+        plural_s = "" if non_plottable == 1 else "s"
+        print('{0} non_plottable objects (callable{1})'.format(
+            non_plottable, plural_s))
+    if plottable > 0:
+        plural_s = "" if plottable == 1 else "s"
+        print('{} polygon{} plotted'.format(plottable, plural_s))
+
+plot_rectangles = plot_polygons # for backwards compatibility
+
 # fast enough so far:
 def find_polygon(x, y, polygons):
     """ return index of the first polygon (in a list of polygons) containing (x,y).
